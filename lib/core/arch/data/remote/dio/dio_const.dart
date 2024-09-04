aa58486a751416c61fe4344b8ca3fa9b5009cd5d
DemeoStep <demeo.alex@gmail.com>
2024-09-04 17:38:06 +0300

    feat: BLoC

--- lib/core/arch/data/remote/dio/dio_const.dart
+++
@@ -1,8 +0,0 @@
-class DioConst {
-  static const String jsonPlaceholderInstance = 'jsonplaceholder';
-  static const String timeApiInstance = 'timeApi';
-
-  static const String jsonPlaceholderBaseUrl =
-      'https://jsonplaceholder.typicode.com';
-  static const String timeApiBaseUrl = 'http://worldclockapi.com';
-}
--- lib/core/arch/domain/entity/failure/canceled_request_failure.dart
+++
@@ -1,5 +0,0 @@
-import 'package:onix_flutter_core/core/arch/domain/entity/failure/failure.dart';
-
-class CanceledRequestFailure implements FeatureFailure {
-  const CanceledRequestFailure();
-}
