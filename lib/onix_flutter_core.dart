6b760a925250dff7ddec583acb32c170500e8c46
Ivan <vanek975@gmail.com>
2024-09-26 19:23:42 +0300

    feat(Updates): Refomat code

--- lib/onix_flutter_core.dart
+++ lib/onix_flutter_core.dart
@@ -1,6 +1,5 @@
 library onix_flutter_core;
 
-// Local
 export 'src/data/local/prefs/base_preferences.dart';
 export 'src/data/remote/base/base_api_client.dart' show BaseApiClient;
 export 'src/data/remote/base/http_status.dart' show HttpStatus;
@@ -9,14 +8,12 @@ export 'src/data/remote/base/map_common_server_error.dart'
 export 'src/data/remote/connection_checker/connection_checker.dart';
 export 'src/data/remote/dio/api_client.dart' show ApiClient;
 export 'src/data/remote/dio/dio_client_module.dart' show DioClientModule;
-// Networking
 export 'src/data/remote/dio/dio_request_processor/dio_request_processor.dart'
     show DioRequestProcessor, OnRequest, OnResponse;
 export 'src/data/remote/dio/dio_request_processor/dio_request_processor_impl.dart';
 export 'src/data/remote/dio/interceptor/cache_interceptor.dart';
 export 'src/data/remote/dio/params/api_client_params.dart' show ApiClientParams;
 export 'src/domain/common/converter/mapper.dart';
-// Models
 export 'src/domain/entity/common/data_response.dart' show DataResponse;
 export 'src/domain/entity/common/operation_status.dart' show OperationStatus;
 export 'src/domain/entity/common/result.dart' show Result;
